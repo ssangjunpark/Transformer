110955c6bc7c368d1d932dfaad256c54d2162d6c
ssangjunpark <06park.sangjun@gmail.com>
2025-12-23 18:37:46 -0500

    prediction logic change

---
+++ decoder.py
@@ -0,0 +1,17 @@
+from transformer import Transformer, PositionalEncoding
+import torch
+import torch.nn as nn
+
+
+class Decoder(nn.Module):
+    def __init__(self, num_embed, d_embed, d_qk, d_v, d_model, n_att, n_transformers, max_length, n_classes):
+        super().__init__()
+        assert d_model == d_embed
+
+        self.embedding = nn.Embedding(num_embed, d_embed)
+        self.transformers = nn.ModuleList([Transformer(d_model, d_qk, d_v, n_att) for _ in range(n_transformers)])
+        self.positional_encoding = PositionalEncoding(d_model, max_length)
+        self.lin = nn.Linear(d_model, n_classes)
+
+    def forward():
+        pass
